f66aa6bc3f79c1a45329c77e3a53d3bd9c797b06
khushihappiness <74294754+khushihappiness@users.noreply.github.com>
2021-09-10 14:53:24 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 noseX=0;
 noseY=0;
+leftWristX=0;
+rightWrist=0;
+difference=0;
 
 function setup()
 {
@@ -16,7 +19,7 @@ poseNet.on('pose',gotPoses);
 function draw()
 {
     background('#F37694')
-    square(noseX, noseY, 100);//usually used to draw a square
+    text( "Khushi" ,noseX, noseY, difference);//usually used to draw a text
     fill('#6CF1D1');
     stroke('#090909');
 }
@@ -35,5 +38,11 @@ function gotPoses(results)
         noseX=results[0].pose.nose.x;
         noseY=results[0].pose.nose.y;
         console.log("noseX=" + noseX + "noseY=" + noseY);
+
+        leftWristX=results[0].pose.leftWrist.x;
+        rightWristX=results[0].pose.rightWrist.x;
+
+        difference=floor(leftWristX-rightWristX);
+        console.log(" leftWristX" + leftWristX + " rightWristY" + rightWristX + " difference" + difference)
     }
 }
